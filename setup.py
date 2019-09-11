565b429436d7a92e01c6d8b9f9104ec9c4976548
Cory Goates <cory.goates@aggiemail.usu.edu>
2019-09-11 06:44:58 -0600

    Added setup file.

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""AirfoilDatabase: A python module for creating databases of complex airfoils."""
+
+from setuptools import setup
+import os
+
+setup(name = 'airfoil_db',
+    version = '1.0',
+    description = "AirfoilDatabase: A Python module for creating databases of complex airfoils",
+    url = 'https://github.com/usuaero/AirfoilDatabase',
+    author = 'usuaero',
+    author_email = 'doug.hunsaker@usu.edu',
+    install_requires = ['numpy', 'scipy', 'pytest', 'matplotlib', 'pandas', 'numpy-stl'],
+    python_requires ='>=3.6.0',
+    license = 'MIT',
+    packages = ['airfoil_db'],
+    zip_safe = False)
