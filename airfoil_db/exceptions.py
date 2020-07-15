02e7674208c649b1adb0722ea4b0a1b8e5f5d3f5
Cory Goates <cory.goates@aggiemail.usu.edu>
2020-07-15 12:20:15 -0600

    Simplified error documentation.

--- airfoil_db/exceptions.py
+++ airfoil_db/exceptions.py
@@ -1,13 +1,29 @@
 """Custom exceptions used in the airfoil class."""
 
 class DatabaseBoundsError(Exception):
-    """An exception thrown when the inputs to the airfoil database fall outside the database bounds."""
+    """An exception thrown when the inputs to the airfoil database fall outside the database bounds.
 
-    def __init__(self, airfoil, exception_indices, inputs_dict, message="The inputs to the airfoil database fell outside the bounds of available data."):
+    Members
+    -------
+
+    airfoil : str
+        The name of the airfoil for which this exception occurred.
+
+    inputs_dict : dict
+        The arguments passed to the airfoil.
+
+    exception_indices : list
+        The indices at which the arguments fell outside the database bounds.
+
+    message : str
+        A message about the error.
+    """
+
+    def __init__(self, airfoil, exception_indices, inputs_dict):
         self.airfoil = airfoil
         self.exception_indices = exception_indices
         self.inputs_dict = inputs_dict
-        self.message = message
+        self.message = "The inputs to the airfoil database fell outside the bounds of available data."
         super().__init__(self.message)
 
 
---
+++ docs/source/error_handling.rst
@@ -0,0 +1,8 @@
+Error Handling
+==============
+
+The following custom errors are defined in AirfoilDatabase
+
+.. automodule:: airfoil_db
+.. autoclass:: DatabaseBoundsError
+   :members:
