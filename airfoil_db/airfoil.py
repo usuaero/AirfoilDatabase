Fixed camber line calculation for symmetric airfoils.

airfoil_db/airfoil.py
@@ -421,7 +421,8 @@ class Airfoil:
             dyc_dx = np.gradient(y_c, x_c, edge_order=camber_deriv_edge_order)
             
             # Determine slope of lines perpendicular to the camber
-            b = -1.0/dyc_dx
+            with np.errstate(divide="ignore"):
+                b = -1.0/dyc_dx
 
             # Loop through points on the camber line to find where their normal intersects the outline
             for i in range(num_camber_points):
@@ -516,7 +517,8 @@ class Airfoil:
         y_c = self._camber_line(x_space)
         dyc_dx= np.gradient(y_c, x_space)
         self._camber_deriv = interp.UnivariateSpline(x_space, dyc_dx, k=5, s=1e-10)
-        b = -1.0/dyc_dx
+        with np.errstate(divide="ignore"):
+            b = -1.0/dyc_dx
 
         # Find points on the surface to determine the thickness
         if self._verbose: print("Calculating thickness distribution...", end="")
@@ -655,7 +657,10 @@ class Airfoil:
 
 
     def _distance(self, x0, y0, x, y, b):
-        return (-b*x0+y0+b*x-y)/np.sqrt(b*b+1)
+        if not np.isnan(b) and not np.isinf(b):
+            return (-b*x0+y0+b*x-y)/np.sqrt(b*b+1)
+        else:
+            return abs(x0-x)
 
 
     def check_against_NACA(self, naca_des):

dev/test.py
@@ -4,7 +4,9 @@ import numpy as np
 import math as m
 from mpl_toolkits.mplot3d import Axes3D
 
-geometry_file = "dev/uCRM-9_wr0_xfoil.txt"
+#geometry_file = "dev/uCRM-9_wr0_xfoil.txt"
+#geometry_file = "dev/symmetric.dat"
+geometry_file = "dev/MMXX0700cTE4.muxf"
 #geometry_file = "dev/64A204.txt"
 #geometry_file = "dev/NACA_9412_geom.txt"
 #geometry_file = "dev/NACA_0012_geom.txt"
@@ -12,14 +14,14 @@ airfoil_input = {
     "type" : "database",
     "geometry" : {
         "outline_points" : geometry_file
-        #"NACA" : "9412",
+        #"NACA" : "0012",
         #"NACA_closed_te" : True
     },
     "trailing_flap_type" : "parabolic"
 }
 
-airfoil = adb.Airfoil("test_airfoil", airfoil_input)
-#airfoil.get_outline_points(trailing_flap_fraction=0.3, trailing_flap_deflection=np.radians(0.0), plot=True)
+airfoil = adb.Airfoil("test_airfoil", airfoil_input, verbose=True)
+#airfoil.get_outline_points(trailing_flap_fraction=0.3, trailing_flap_deflection=np.radians(0.0), export="dev/symmetric.dat")
 
 dofs = {
     "alpha" : {
@@ -79,9 +81,9 @@ Cm_fit_orders = {
 }
 
 # Generate or import fits
-airfoil.generate_polynomial_fit(CL_degrees=CL_fit_orders, CD_degrees=CD_fit_orders, Cm_degrees=Cm_fit_orders)
+#airfoil.generate_polynomial_fit(CL_degrees=CL_fit_orders, CD_degrees=CD_fit_orders, Cm_degrees=Cm_fit_orders)
 #airfoil.generate_polynomial_fit(CL_degrees="auto", CD_degrees="auto", Cm_degrees="auto", max_order=6)
-airfoil.export_polynomial_fits(filename="fits.json")
+#airfoil.export_polynomial_fits(filename="fits.json")
 #airfoil.import_polynomial_fits(filename="database.json")
 
 # Compare interpolation and fits